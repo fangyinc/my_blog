7119c47bf676597db980904cef0f85398afc6af9
staneyffer <staneyffer@gmail.com>
2017-08-07 23:53:23 +0800

    修改部分依赖文件

--- fabfile.py
+++ fabfile.py
@@ -43,5 +43,5 @@ def deploy():
             sudo("git clone git@github.com:staneyffer/my_blog.git %s" % code_dir)
     with cd(code_dir):
         sudo("git pull")
-        #sudo("systemctl restart my_blog.service")
+        sudo("systemctl restart my_blog.service")
         sudo("systemctl restart nginx")
